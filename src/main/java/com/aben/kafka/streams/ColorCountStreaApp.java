2b57ca882825f88da2c45ea2b13d7222bbfcb7cb
abouhoudayfa <benchamayoub@gmail.com>
2018-07-22 11:49:08 +0200

    refactor kafka streams app java count color in input

--- src/main/java/com/aben/kafka/streams/ColorCountStreaApp.java
+++ src/main/java/com/aben/kafka/streams/ColorCountStreaApp.java
@@ -13,7 +13,19 @@ import java.util.Arrays;
 import java.util.Properties;
 
 /**
- * Created by hadoop on 7/17/18.
+ * Created by abouhoudayfa on 7/17/18.
+ cd /usr/local/kafka
+ bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic color-input-color
+ bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic color-output-color
+ bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic user-key-color
+ //list topics
+ bin/kafka-topics.sh --list --zookeeper localhost:2181
+ //create consumer
+ bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic word-count-output --from-beginning --property print.key=true --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer --property value.deserializer=org.apache.kafka.common.serialization.LongDeserializer
+ //create producer
+ cd /usr/local/kafka
+ bin/kafka-console-producer.sh --broker-list localhost:9092 --topic word-count-input   --property "parse.key=true" \
+ --property "key.separator=,"
  */
 public class ColorCountStreaApp {
     public static void main(String[] args) {
@@ -23,20 +35,18 @@ public class ColorCountStreaApp {
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
 
         KStreamBuilder builder = new KStreamBuilder();
-        // 1 - stream from Kafka
-
         KStream<String, String> streamColors = builder.stream("color-count-input");
-        KStream<String, String> colorCounts = streamColors
+        streamColors
                 .filter((name, color) -> !name.isEmpty())
-                .selectKey((name, color) -> color) // (color, color)
-                .mapValues(inputStream -> inputStream.toLowerCase())
-                .filter((name, color) -> color.equals("red") || color.equals("blue") || color.equals("green"));
+                .selectKey((name, color) -> color)
+                .mapValues(color -> color.toLowerCase())
+                .filter((name, color) -> color.equals("red") || color.equals("blue") || color.equals("green"))//Arrays.asList(
+                .to("user-key-color");
 
-        colorCounts.to("colorCounts");
-
-        KTable<String, String> usersAndColorsTable = builder.table("colorCounts");
+        KTable<String, String> usersAndColorsTable = builder.table("user-key-color");
 
         KTable<String, Long> favoriteColors = usersAndColorsTable
                 .groupBy((name, color) -> new KeyValue<>(color, color))
@@ -44,6 +54,7 @@ public class ColorCountStreaApp {
         favoriteColors.to(Serdes.String(), Serdes.Long(), "color-count-output");
 
         KafkaStreams streams = new KafkaStreams(builder, config);
+        streams.cleanUp();
         streams.start();
 
         //print the toplogy
